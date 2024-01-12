49caf1911b4949c774e023922f22d8a691991b17
Ivan Luis Forner <iforner@ype.ind.br>
2024-01-12 14:09:39 -0300

    aula1

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,24 @@
 package br.com.alura.screenmatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/*
+Como estamos criando uma aplicação que vai rodar em linha de comando,
+iniciamos com a implementação do CommandLineRunner e sobrescrevemos o método run().
+*/
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!");
 	}
 
 }
